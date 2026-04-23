8a381d7f8acbaa5a4971ea7e49438cbff9ddc96e
Kelvin <quantyrexmarkets@gmail.com>
2026-04-23 22:24:41 +0100

    fix: Render deployment config

--- pilot.js
+++ pilot.js
@@ -1,19 +1,23 @@
 #!/usr/bin/env node
 
-console.log('');
-console.log('  🛩️  TermuxPilot v1.0.0');
-console.log('  ──────────────────────');
-console.log('');
+const isRender = process.env.RENDER || false;
 
-// Start the server
+if(!isRender){
+  console.log('');
+  console.log('  TermuxPilot v2.0.0');
+  console.log('  ──────────────────────');
+  console.log('');
+}
+
 require('./src/server');
 
-// Auto-open browser after 2 seconds
-setTimeout(() => {
-  const { exec } = require('child_process');
-  exec('am start -a android.intent.action.VIEW -d http://localhost:8000', (err) => {
-    if (err) {
-      exec('termux-open-url http://localhost:8000', () => {});
-    }
-  });
-}, 2000);
+if(!isRender){
+  setTimeout(() => {
+    const { exec } = require('child_process');
+    exec('am start -a android.intent.action.VIEW -d http://localhost:3000', (err) => {
+      if (err) {
+        exec('termux-open-url http://localhost:3000', () => {});
+      }
+    });
+  }, 2000);
+}
